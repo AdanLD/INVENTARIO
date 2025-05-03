410f0a9696b20fe6c3990ff782c6548c66120028
PC <l231140008@surnayarit.tecnm.mx>
2025-05-03 01:11:51 -0700

    Uso del nugget FontAwesome.Sharp en la capa presentacion.

--- CAPA-PRESENTACION/LOGIN.Designer.cs
+++ CAPA-PRESENTACION/LOGIN.Designer.cs
@@ -28,19 +28,91 @@
         /// </summary>
         private void InitializeComponent()
         {
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            groupBox1 = new GroupBox();
+            label3 = new Label();
+            groupBox1.SuspendLayout();
             SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(163, 31);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(372, 23);
+            textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(163, 84);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(372, 23);
+            textBox2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(163, 13);
+            label1.Name = "label1";
+            label1.Size = new Size(56, 15);
+            label1.TabIndex = 2;
+            label1.Text = "USUARIO";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(163, 66);
+            label2.Name = "label2";
+            label2.Size = new Size(84, 15);
+            label2.TabIndex = 3;
+            label2.Text = "CONTRASEÑA";
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(textBox1);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(textBox2);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Location = new Point(12, 110);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(554, 196);
+            groupBox1.TabIndex = 4;
+            groupBox1.TabStop = false;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(26, 28);
+            label3.Name = "label3";
+            label3.Size = new Size(48, 15);
+            label3.TabIndex = 5;
+            label3.Text = "LOG IN ";
             // 
             // LOGIN
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(label3);
+            Controls.Add(groupBox1);
             Name = "LOGIN";
             StartPosition = FormStartPosition.CenterScreen;
             Load += LOGIN_Load;
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Label label1;
+        private Label label2;
+        private GroupBox groupBox1;
+        private Label label3;
     }
 }
