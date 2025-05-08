c2573fe37fce617d201dc0b62ba1778e032151b5
PC <l231140008@surnayarit.tecnm.mx>
2025-05-08 14:11:04 -0700

    Las relaciones entre las columnas de Permiso y la BD dentro de CD_Permiso fueron asigndas.

--- CAPA_DATOS/CD_Permisos.cs
+++ CAPA_DATOS/CD_Permisos.cs
@@ -27,7 +27,7 @@ namespace CAPA_DATOS
                     query.AppendLine("INNER JOIN TB_Usuario U on U.cargo_ID = C.cargo_ID");
                     query.AppendLine("WHERE U.usuario_ID = @usuario_ID;"); //Usa al usuario_ID como filtro (Adan).
 
-                    
+
                     SQLiteCommand cmd = new SQLiteCommand(query.ToString(), cn); //El query ahora es un string (Adan).
                     cmd.Parameters.AddWithValue("@usuario_ID", usuario_ID); //Agrega el parametro de usuario_ID que remplazara al otro (Adan).
                     cmd.CommandType = System.Data.CommandType.Text;
@@ -38,31 +38,28 @@ namespace CAPA_DATOS
                     {
                         while (sqdr.Read())
                         {
-                            lista.Add(new Usuario() //Quizas aun deba modificar algunas columnas (Adan).
+                            lista.Add(new Permiso()
                             {
-                                usuario_ID = Convert.ToInt32(sqdr["usuario_ID"]),
-                                correo_Usuario = sqdr["correo_Usuario"].ToString(),
-                                contraseña_Usuario = sqdr["contrasena_Usuario"].ToString(),
-                                nombre_Paterno_Usuario = sqdr["nombre_paterno_Usuario"].ToString(),
-                                numero_telefonico_Usuario = sqdr["numero_telefonico_Usuario"].ToString(),
-                                ObjCargo = new Cargo(),
-                                fecha_Creacion_Usuario = DateOnly.FromDateTime(DateTime.Now),
-                                nombre_Materno_Usuario = sqdr["nombre_Materno_Usuario"].ToString(),
-                                nombre_Usuario = sqdr["nombre_Usuario"].ToString(),
-                                hora_Creacion_Usuario = TimeOnly.FromDateTime(DateTime.Now),
-                                estado_Actividad_Usuario = Convert.ToBoolean(sqdr["estado_Actividad_Usuario"]),
-                                documento_Usuario = sqdr["documento_Usuario"].ToString()
-
+                                permiso_ID = Convert.ToInt32(sqdr["permiso_ID"]),
+                                nombre_Permiso = sqdr["nombre_Permiso"].ToString(),
+                                codigo_Permiso = sqdr["nombre_Permiso"].ToString(),
+                                fecha_Creacion_Permiso = DateOnly.FromDateTime(DateTime.Now),
+                                hora_Creacion_Permiso = TimeOnly.FromDateTime(DateTime.Now),
+                                ObjCargo = new Cargo()
+                                {
+                                    cargo_ID = Convert.ToInt32(sqdr["cargo_ID"]),
+                                }
                             });
                         }
                     }
                 }
                 catch (Exception)
                 {
-                    lista = new List<Usuario>(); //Lista vacia (Adan).
+                    lista = new List<Permiso>(); //Lista vacia (Adan).
                     throw;
                 }
             }
             return lista;
-        }
-}
+        } 
+      }
+    }
