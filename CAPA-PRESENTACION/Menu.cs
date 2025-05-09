f3482ef5a424b0dc1f77e3be5c4cfcd6c2b1c5a1
PC <l231140008@surnayarit.tecnm.mx>
2025-05-08 18:02:09 -0700

    Adaptacion
    Referencia a la CAPA_NEGOCIO en el formulario Menu.cs .
    
    Se ingreso una  lista de permisos lista los permisos del usuario actual en el formulario Menu.cs .

--- CAPA-PRESENTACION/Menu.cs
+++ CAPA-PRESENTACION/Menu.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using CAPA_ENTIDAD;
+using CAPA_NEGOCIO;
 using FontAwesome.Sharp;
 using FontAwesome;
 
@@ -26,7 +27,10 @@ namespace CAPA_PRESENTACION
         //static string nombreC = nombreC;
         private void Menu_Load(object sender, EventArgs e)
         {
+            List<Permiso> listaPermisos = new CN_Permiso().Listar(UsuarioActual.usuario_ID); //Se asigna a la listaPermisos el resultado de la consulta de permisos para el usuario actual (Adan).
+
             string nombreC = UsuarioActual.nombre_Usuario + " " + UsuarioActual.nombre_Paterno_Usuario + " " + UsuarioActual.nombre_Materno_Usuario; //Cadena con el nombre completo (Adan).
+
             this.Text = $"Usuario: {nombreC}."; //Cambia la propiedad del text por el nombre completo del usuario (Adan).
         }
 
