5747c822daa9452ee25ffdb8bbd40aabd81f4fd2
ABDIEL KIBSAIM LUCIAN HERNANDEZ <l231140042@surnayarit.tecnm.mx>
2025-05-13 20:21:41 -0700

    este es la ultima que subo solo tiene un boton

--- CAPA-PRESENTACION/FormInventario.Designer.cs
+++ CAPA-PRESENTACION/FormInventario.Designer.cs
@@ -28,23 +28,12 @@
         /// </summary>
         private void InitializeComponent()
         {
-            dataGridView1 = new DataGridView();
             button1 = new Button();
-            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
-            // 
-            // dataGridView1
-            // 
-            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Location = new Point(524, 46);
-            dataGridView1.Name = "dataGridView1";
-            dataGridView1.RowHeadersWidth = 51;
-            dataGridView1.Size = new Size(340, 324);
-            dataGridView1.TabIndex = 0;
             // 
             // button1
             // 
-            button1.Location = new Point(287, 111);
+            button1.Location = new Point(23, 21);
             button1.Name = "button1";
             button1.Size = new Size(94, 29);
             button1.TabIndex = 1;
@@ -57,17 +46,13 @@
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1148, 554);
             Controls.Add(button1);
-            Controls.Add(dataGridView1);
             Name = "FormInventario";
             Text = "FormInventario";
             Load += FormInventario_Load;
-            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
-
-        private DataGridView dataGridView1;
         private Button button1;
     }
 }
